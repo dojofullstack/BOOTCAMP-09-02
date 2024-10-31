f5d4799f72261e5cf4e3bc047ac9cc60ddaa451d
dojofullstack <hola@dojofullstack.com>
2024-10-30 21:54:11 -0500

    update

---
+++ FRONTEND/JavaScript/assets/js/app.js
@@ -0,0 +1,71 @@
+
+
+// console.log("Hola Mundo!!!");
+
+console.log("modulo externo app.js");
+
+
+
+// formas de definir variables
+
+// var
+// let
+// const
+
+
+// formas de definir variables con var
+
+var producto = "samsung galaxy";
+
+var precio;
+
+
+console.log(producto);
+
+console.log(precio);
+
+
+producto = "samsung galaxy V2";
+
+precio = 95;
+
+
+console.log(producto);
+
+console.log(precio);
+
+
+// formas de definir variables con let
+
+let username = "juana";
+let email = "jauana@gmail.com";
+let password;
+
+console.log(username);
+console.log(email);
+console.log(password);
+
+
+
+username = "juana2024";
+email = "jauana2024@gmail.com";
+
+
+console.log(username);
+console.log(email);
+
+
+// formas de definir datos con const
+
+
+const token = "0101010011029012010290129";
+const API_USER = "http://api.dojo.com/v1/usuarios";
+// const ACCESS_TOKEN; ESTA LINEA GENERAR UN ERROR
+
+console.log(token);
+console.log(API_USER);
+
+// token = "28282828";
+// API_USER = "http://api.dojo.com/v2/usuarios";
+
+
---
+++ FRONTEND/JavaScript/assets/js/tipos_datos.js
@@ -0,0 +1,38 @@
+
+
+// Javascript tipado dinamico
+
+// string
+// numerales
+// booleanos
+// array
+// objetos
+
+
+let auto = "auto tesla";
+let moto = " \n  yamaha   \n siguiente parrago \n dede\n";
+
+
+
+console.log( typeof auto);
+
+console.log( typeof moto);
+
+
+
+auto.includes("teslo");
+
+auto.replace("tesla", "hyundi");
+
+auto.replace("teslooo", "hyundi");
+
+moto.trim()
+
+auto.indexOf("tesla");
+
+
+auto.toUpperCase();
+
+let text = "How-are-you-doing-today?";
+const myArray = text.split("-");
+console.log(myArray.join(" "));
